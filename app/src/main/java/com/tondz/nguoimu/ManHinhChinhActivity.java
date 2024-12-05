f8863596de90b7d37fdb98ed359a5a46182da6e2
tondt <tondt@dyno.vn>
2024-12-05 21:52:19 +0700

    add

--- app/src/main/java/com/tondz/nguoimu/ManHinhChinhActivity.java
+++ app/src/main/java/com/tondz/nguoimu/ManHinhChinhActivity.java
@@ -60,6 +60,24 @@ public class ManHinhChinhActivity extends AppCompatActivity {
                 startActivity(new Intent(getApplicationContext(), MainActivity.class));
             }
         });
+        findViewById(R.id.btnMic).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent
+                        = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                        RecognizerIntent.LANGUAGE_MODEL_FREE_FORM);
+                intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE,
+                        Locale.getDefault());
+                intent.putExtra(RecognizerIntent.EXTRA_PROMPT, "Speak to text");
+
+                try {
+                    startActivityForResult(intent, REQUEST_MIC);
+                } catch (Exception e) {
+                    Toast.makeText(ManHinhChinhActivity.this, "Thiết bị không hỗ trợ tính năng này", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
     }
 
     private void actionMic(String text) {
